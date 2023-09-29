ee488bcd4368edd62876110612fc892b7a9778c7
Kitso Gidion Namane <kitsogideonnamane@gmail.com>
2023-09-29 23:24:09 +0200

    feat(two pointer): is valid palindrome

--- 005_is_valid_palindrome/main.go
+++ 005_is_valid_palindrome/main.go
@@ -6,27 +6,47 @@ import (
 )
 
 func isPalindrome(s string) bool {
-
 	sLower := []rune(strings.ToLower(s))
 	first, last := 0, len(sLower)-1
 
-	for first <= last {
+	fmt.Println(sLower)
+
+	for first < last {
+
+		for first < last && !isAlphaNumeric(sLower[first]) {
+			first++
+		}
+
+		for first < last && !isAlphaNumeric(sLower[last]) {
+			last--
+		}
+
 		left := sLower[first]
 		right := sLower[last]
 
-		isLetter := left >= 97 && left <= 122
-		isNumber := left >= 48 && left <= 57
-
-		if isLetter && isNumber && left != right {
+		if left != right {
+			fmt.Printf("left %v - index %v\n", left, first)
+			fmt.Printf("right %v - index %v\n", right, last)
 			return false
 		}
 
 		first++
 		last--
 	}
+
 	return true
 }
 
+func isAlphaNumeric(r rune) bool {
+	isLetter := r >= 97 && r <= 122
+	isNumber := r >= 48 && r <= 57
+
+	return isLetter || isNumber
+}
+
 func main() {
-	fmt.Println(isPalindrome("A man, a plan, a canal, Panama!"))
+	fmt.Println(isPalindrome("ra aar"))
+	fmt.Println(isPalindrome("race a car"))
+	fmt.Println(isPalindrome("A man, a plan, a canal: Panama"))
+	fmt.Println(isPalindrome(" "))
 }
